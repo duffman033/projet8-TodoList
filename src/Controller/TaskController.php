82d31dd775c4534aa5653dc662f8f4878212f23b
Bastien Moreau <bmoreau72@gmail.com>
2023-01-23 20:32:11 +0100

    Updates

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/tasks', name: 'app_task_list')]
+    public function list(TaskRepository $taskRepository)
+    {
+        return $this->render('task/list.html.twig', [
+            'tasks' => $taskRepository->findAll()
+        ]);
+    }
+
+    #[Route('/tasks/create', name: 'app_task_create', methods: ['GET','POST'])]
+    public function create(ManagerRegistry $manager, Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $manager->getManager();
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('app_task_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/tasks/{id}/edit', name: 'app_task_edit', methods: ['GET','POST'])]
+    public function edit(ManagerRegistry $manager, Task $task, Request $request)
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->getManager()->flush();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('app_task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/tasks/{id}/toggle', name: 'app_task_toggle', methods: ['GET','POST'])]
+    public function toggleTask(ManagerRegistry $manager, Task $task)
+    {
+        $task->toggle(!$task->isDone());
+        $manager->getManager()->flush();
+
+        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+        return $this->redirectToRoute('app_task_list');
+    }
+
+    #[Route('/tasks/{id}/delete', name: 'app_task_delete', methods: ['POST'])]
+    public function deleteTask(ManagerRegistry $manager, Task $task): Response
+    {
+        $em = $manager->getManager();
+        $em->remove($task);
+        $em->flush();
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('app_task_list');
+    }
+}
